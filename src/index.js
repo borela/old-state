12871474ac5e1685ef0c866ec74d3e3fdc274d59
Alexandre Borela <alexandre@borela.tech>
2017-06-13 15:05:52 -0300

    Add implementation

---
+++ src/index.js
@@ -0,0 +1,34 @@
+// Licensed under the Apache License, Version 2.0 (the “License”); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an “AS IS” BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+import { Component } from 'react'
+
+export function decorate(targetComponent:Component) {
+  let prototype = targetComponent.prototype
+
+  Object.defineProperty(prototype, 'oldState', {
+    get() {
+      return this._oldState || this.props
+    }
+  })
+
+  let oldComponentWillUpdate = prototype.componentWillUpdate
+  prototype.componentWillUpdate = function(nextProps, nextState) {
+    this._oldState = this.state
+    if (oldComponentWillUpdate)
+      oldComponentWillUpdate(nextProps, nextState)
+  }
+
+  return targetComponent
+}
+
+export default decorate
